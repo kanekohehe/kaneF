70357c61eabc39c076737ced234212e8c529fc7b
U-PS11-10\vantn <vantn@PS11-10.(none)>
2014-01-18 15:09:24 +0900

    sF

--- std_in_out.c
+++ std_in_out.c
@@ -1,18 +1,56 @@
 #include <stdio.h>
+int check(char c)
+{
+	if (c == '+'|c == '-'|c == '='|c == ' ')
+		return 1;
+	else
+		return 0;
+}
 main ()
 {
+	int c;
+	int i;
 	char st[123];
-	int i; 
-	// “ü—Í
-	gets(st);
-	//scanf("%s",st);
-	// o—Í
-	printf("[");
-	
-	if(&st != " ")
+	//st[0] = '[';						/*“ª‚ÌŠ‡ŒÊ*/
+	//i = 1;
+    while ((c = getchar(  )) != EOF)	/* •¶š‚Ì“ü—Í */
 	{
-		printf(&st);
-	}
-	printf("]");
-
+		if (c == '\n')					/*Enter‚Å”²‚¯*/
+		{
+			st[i] = ']';
+            break;               
+		}
+		if (c == ' ')					/*SPACE*/
+		{
+		}
+		else if (c == '+'|c == '-'|c == '=')/* + - = */
+		{
+			st[i] = '[';
+			i++;
+			st[i] = c;
+			i++;
+			st[i] = ']';
+			i++;
+		}
+		else							/*•¶š*/
+		{
+			if(i != 0)
+			{
+				if(check(st[i-1]))
+				{
+					st[i] = '[';
+					i++;
+				}
+			}
+			else
+			{
+				st[i] = '[';
+				i++;
+			}
+			st[i] = c;
+			i++;
+		}
+		
+    }
+	printf("%s",st); 
 }
