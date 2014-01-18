5191999151a62636c99e855ddda6848eea086214
U-PS11-10\vantn <vantn@PS11-10.(none)>
2014-01-18 16:32:02 +0900

    c.c

--- c.c
+++ c.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main ()
 {
-	int a = 1;
+	int a = 4;
 	int b = 2;
 	int c = (a | b);
 	int d = (a || b);
 	printf("a = %d\n",a);
-	printf("a = %d\n",b);
-	printf("a = %d\n",c);
-	printf("a = %d\n",d);
+	printf("b = %d\n",b);
+	printf("c = %d\n",c);
+	printf("d = %d\n",d);
 	return 0;
 }
