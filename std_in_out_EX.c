4828abed12f1a2f60d0ec3c6991541af6c9c7d3a
U-PS11-10\vantn <vantn@PS11-10.(none)>
2014-01-18 19:08:03 +0900

    EX commit

--- std_in_out_EX.c
+++ std_in_out_EX.c
@@ -3,9 +3,10 @@ int main ()
 {
 	int c;
 	int inword = 0;
+	int inspace = 1;
 	while((c = fgetc(stdin)) != EOF)
 	{
-		if(c == ' ' || c == '	')
+		if(c == ' ' || c == '	' || c == '\n')
 		{
 			if(inword)
 			{
@@ -13,15 +14,19 @@ int main ()
 				putchar('\n');
 			}
 			inword = 0;
+			inspace = 1;
 		}
 		else
 		{
+			if(inspace)
+			{
+				putchar('[');
+			}
+			inspace = 0;
 			putchar(c);
 			inword = 1;
-		}
 
-		if(c=='\n')
-			break; 
+		}
 	}
 	return 0;
 }
