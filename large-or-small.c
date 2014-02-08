225b392a9501815e8c48c15ab251cea4d2466a9c
U-PS11-10\vantn <vantn@PS11-10.(none)>
2014-02-08 16:03:37 +0900

    large-or-small.c commit

--- large-or-small.c
+++ large-or-small.c
@@ -3,14 +3,16 @@
 
 int main()
 {
-  int value = (arc4random() % 5) + 1;
+  int value = (arc4random() % 10) + 1;
+  int counter = 0;
   while (1) {
     int answer;
     printf("value = %d\n", value);
     printf("? ");
+	counter += 1;
     scanf("%d", &answer);
     if (value == answer) {
-      printf("Congratulations!!\n");
+      printf("%dChallenge!! Congratulations!!\n", counter);
       break;
     } else if (value < answer) {	// “š‚¦‚ª‘å‚«‚¢‚Æ‚«
       printf("small...\n");
