0ee4fa1421aa01884a35d1144b151a07ac1f3d22
U-PS11-10\vantn <vantn@PS11-10.(none)>
2014-02-08 15:22:14 +0900

    large-or-small.c commit

--- large-or-small.c
+++ large-or-small.c
@@ -1,30 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main ()
-{
+int main (){
 	int value = (arc4random() % 5) + 1;
 	int answer;
-	while(1)
-	{
+	while(1){
 		printf("value = %d\n",value);
 		printf("?");
 		scanf("%d",&answer);
-		if(value == answer)
-		{
+		if(value == answer){
 			printf("Congratulations!!");
 			break;
 		}
-		else if(value < answer)	// “š‚¦‚ª‘å‚«‚¢‚Æ‚«
-		{
+		else if(value < answer){	// “š‚¦‚ª‘å‚«‚¢‚Æ‚«
 			printf("small...");
 		}
-		else					// “š‚¦‚ª¬‚³‚¢‚Æ‚«
-		{
+		else{					// “š‚¦‚ª¬‚³‚¢‚Æ‚«
 			printf("large...");
 		}
-
 	}
-	
 	return 0;
 }
